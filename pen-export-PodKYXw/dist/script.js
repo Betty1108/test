90b2e91892391b2c6c43a78c533155299a89a1c6
Betty1108 <130964637+Betty1108@users.noreply.github.com>
2023-05-29 14:55:34 +0800

    Add files via upload

--- pen-export-PodKYXw/dist/script.js
+++ pen-export-PodKYXw/dist/script.js
@@ -1,1 +1,10 @@
 document.write("我是洪瑀婕")
+
+
+var count = 0;
+    var counterElement = document.getElementById("counter");
+
+    function increment() {
+      count+=2;
+      counterElement.textContent = count;
+    }
